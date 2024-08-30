218aa46ff8fa78e5e9bbb37ccaa118a6961bb438
RexxuDesu <130981329+RexxuDesu@users.noreply.github.com>
2024-08-30 12:05:06 +0800

    Update Type.kt

--- java/com/lunr/kabsatrides/ui/theme/Type.kt
+++ java/com/lunr/kabsatrides/ui/theme/Type.kt
@@ -1,6 +1,6 @@
-package com.lunr.kabsatrides3.ui.theme
+package com.lunr.kabsatrides.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
