421ff6ca5519b6c99a45eb4586e066e74b8efbab
RexxuDesu <130981329+RexxuDesu@users.noreply.github.com>
2024-08-30 12:04:48 +0800

    Update Theme.kt

--- java/com/lunr/kabsatrides/ui/theme/Theme.kt
+++ java/com/lunr/kabsatrides/ui/theme/Theme.kt
@@ -1,13 +1,13 @@
-package com.lunr.kabsatrides3.ui.theme
+package com.lunr.kabsatrides.ui.theme
 
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColorScheme
+import androidx.compose.material.dynamicDarkColorScheme
+import androidx.compose.material.dynamicLightColorScheme
+import androidx.compose.material.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun KabsatRides3Theme(
+fun KabsatRidesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
