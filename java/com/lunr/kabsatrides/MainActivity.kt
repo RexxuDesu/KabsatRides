32d87d8b0a6c6d411f5d90c566165cd31902fb5e
RexxuDesu <130981329+RexxuDesu@users.noreply.github.com>
2024-08-31 14:07:16 +0800

    Update MainActivity.kt

--- java/com/lunr/kabsatrides/MainActivity.kt
+++ java/com/lunr/kabsatrides/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.lunr.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.lunr.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.button001)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "Placeholder"
+        }
     }
+
 }
