b1bc1d32cec1f5fddca7f46811669ab8df653dc1
RexxuDesu <130981329+RexxuDesu@users.noreply.github.com>
2024-08-31 15:14:43 +0800

    Update MainActivity.kt

--- java/com/lunr/kabsatrides/MainActivity.kt
+++ java/com/lunr/kabsatrides/MainActivity.kt
@@ -2,17 +2,19 @@ package com.lunr.myapplication
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "You are testing KabsatRides", Toast.LENGTH_LONG).show()
 
         val btnClickMe = findViewById<Button>(R.id.button001)
         btnClickMe.setOnClickListener {
-            btnClickMe.text = "Placeholder"
+            setContentView(R.layout.activity_lobby)
         }
-    }
 
+    }
 }
